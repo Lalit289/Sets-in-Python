271838f9d4678682f8fb4cfdc0d29a70288997eb
Lalit289 <87890683+Lalit289@users.noreply.github.com>
2022-01-28 14:08:23 +0530

    Add files via upload

---
+++ set_tut.py
@@ -0,0 +1,24 @@
+# 6th class 
+clasromA = {"Lalit","Rahul","Daksh","Adesh"}
+clasromB = {"Disha","Sawra","Mahesh"}
+# 7th class 
+newyrs = clasromA.union(clasromB)
+print(newyrs)
+# print(clasromA)
+
+# # add()
+
+# clasrom.add("Prakash")
+
+# # remove()
+
+# clasrom.remove("Rahul")
+
+
+
+
+# tup = set(("Orange","Apple","Grapes"))
+# tup.add("Watermelon")
+
+# print(tup)
+
